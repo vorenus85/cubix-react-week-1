de15310820ba1f7faf40fb985973f90f35ee11a7
Janos Perge <janos.perge@gmail.com>
2023-12-14 22:20:22 +0100

    create recipe project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import './difficulty.css'
+const recipes = [
+  {
+    id: 1,
+    name: "Fried eggs",
+    ingredients: ['2 eggs', 'a pinch of salt and pepper'],
+    instructions: 'Break them, fry them, eat them!',
+    difficulty: 2,
+  },
+  {
+    id: 2,
+    name: "Fruit salad",
+    ingredients: ['1 apple', '2 bananas', '1 orange', '1 grape', '1/3 tablespoon lemon juice', '1/2 cup sugar'],
+    instructions: 'Slice the fruits, add sugar and lemon juice. Mix it gently. Cover and refrigerate until serving',
+    difficulty: 3,
+  },
+  {
+    id: 3,
+    name: "Boiled eggs",
+    ingredients: ['2 eggs', 'a pinch of salt and pepper'],
+    instructions: 'Boil the eggs, cool it, break them, eat them.',
+    difficulty: 1,
+  },
+];
+
+function Recipe({name, difficulty, ingredients, instructions}) {
+
+  let level = '';
+  switch (difficulty) {
+    case 1:
+      level = 'easy'
+      break;
+    case 2:
+      level = 'medium'
+      break;
+    case 3:
+      level = 'nightmare'
+      break;
+  
+    default:
+      break;
+  }
+
+  return (<li>
+            <h2>{name}</h2>
+            <p><b>Ingredients:</b>
+              <ul>
+                {
+                  ingredients.map(ingredient =>{
+                    return (<li>- {ingredient}</li>)
+                  })
+                }
+              </ul>
+            </p>
+            <p><b>Instructions:</b> {instructions}</p>
+            <p><small><b>Difficulty:</b> <span className={`level level-${level}`}>{level}</span></small></p>
+            
+          </li>);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipes</h1>
+      <ul>
+        {
+          recipes.map( recipe => {
+            return (<Recipe name={recipe.name} difficulty={recipe.difficulty} instructions={recipe.instructions} ingredients={recipe.ingredients} key={recipe.id}/>);
+          })
+        }
+      </ul>
     </div>
   );
 }
